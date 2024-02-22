8bac95946893e1a84b4d3212caa84450729dbf3a
PollardK6452 <160648831+PollardK6452@users.noreply.github.com>
2024-02-22 13:32:09 -0500

    Add files via upload

---
+++ P1LAB1_PollardKevin.py
@@ -0,0 +1,9 @@
+# Kevin Pollard
+# 2/22/2024
+# P1LAB1
+# Diplay a welcome message
+
+fname = str(input('Enter your first name: '))
+lname = str(input('Enter your last name: '))
+print("Hello,",fname,lname+"! Welcome to CTI-110")
+
